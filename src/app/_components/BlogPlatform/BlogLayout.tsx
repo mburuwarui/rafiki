8bb3fed12daa79f842f9fa834997cbdb6667b811
mburuwarui <mburuwarui@gmail.com>
2023-11-20 22:30:29 +0300

    contentlayer

--- src/app/_components/BlogPlatform/BlogLayout.tsx
+++ src/app/_components/BlogPlatform/BlogLayout.tsx
@@ -97,7 +97,7 @@ export default async function BlogLayout(
               </Group>
 
               <Text fz="sm" c="dimmed">
-                {props.readingTime}
+                {props.readingTime && props.readingTime.text}
               </Text>
             </Group>
             <Divider variant="dotted" />
