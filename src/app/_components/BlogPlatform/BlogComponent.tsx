775ff506f4e346f734784b5dc7568d8c6aa44ac4
mburuwarui <mburuwarui@gmail.com>
2023-11-20 22:04:17 +0300

    contentlayer

--- src/app/_components/BlogPlatform/BlogComponent.tsx
+++ src/app/_components/BlogPlatform/BlogComponent.tsx
@@ -11,7 +11,7 @@ export function BlogComponent({ props }: { props: Blog }) {
   // Define your custom MDX components.
   const mdxComponents: MDXComponents = {
     // Override the default <a> element to use the next/link component.
-    a: ({ href, children }) => <Link href={href! as string}>{children}</Link>,
+    a: ({ href, children }) => <Link href={href}>{children}</Link>,
     // Add a custom component.
     MyComponent: () => <div>Hello World!</div>,
     Button: ({ children }) => <Button>{children}</Button>,
